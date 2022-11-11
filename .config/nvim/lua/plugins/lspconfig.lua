0ddc67bf2c7d375c80ff6f8cdd982b8ab483a8c1
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-11-11 13:12:13 +0100

    switch to fedora silverblue

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,7 @@
 ------------------ LSP CONFIG -------------------
 -------------------------------------------------
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 --------------------- Bash ----------------------
 require('lspconfig').bashls.setup{}
